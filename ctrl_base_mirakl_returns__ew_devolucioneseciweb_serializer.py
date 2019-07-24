cfc8e6164f8041b2dfa95fad7d965f6da60990e6
Lorena Lopez Lopez <lorena@yeboyebo.es>
2019-07-24 11:57:29 +0200

    Cambios en mirakl returns

--- ctrl_base_mirakl_returns__ew_devolucioneseciweb_serializer.py
+++ ctrl_base_mirakl_returns__ew_devolucioneseciweb_serializer.py
@@ -13,9 +13,8 @@ class DevolucioneseciwebSerializer(DefaultSerializer):
         self.set_string_value("estado", "PENDIENTE")
         self.set_string_value("datosdevol", self.init_data["body"], max_characters=None, skip_replace=True)
         self.set_data_value("recibida", False)
-        self.set_data_value("idtpv_comanda", None)
         self.set_string_value("fechaalta", now[:10])
         self.set_string_value("horaalta", now[-8:])
-        self.set_string_relation("date_created","date_created")
+        self.set_string_relation("date_created", "date_created")
 
         return True
