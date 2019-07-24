0bf16406bb2524cc6d6c959a3a4c89d677c99372
Jesus Zornoza <jesus.yeboyebo@gmail.com>
2019-07-24 12:16:45 +0200

    Cambio campo tarifa grupoclienteb2b para sincro tarifas

--- ctrl_base_magento2_tierprice__tierprice_serializer.py
+++ ctrl_base_magento2_tierprice__tierprice_serializer.py
@@ -6,7 +6,7 @@ class TierpriceSerializer(DefaultSerializer):
     def get_data(self):
         self.set_string_relation("sku", "lsc.descripcion")
         self.set_data_relation("price", "a.pvp")
-        self.set_string_relation("customer_group", "t.grupoclientesb2b")
+        self.set_string_relation("customer_group", "gcb2b.grupoclientesb2b")
         self.set_string_value("price_type", "fixed")
         self.set_data_value("quantity", 1)
         self.set_data_value("website_id", 0)
