07fb95a4cfdfcfb084ecfc8eac4bbae359a88938
xavikortes <javiercortesveliz@gmail.com>
2019-07-30 15:00:36 +0200

    Otro bugfix

--- model_flsyncppal__flsyncppal_def.py
+++ model_flsyncppal__flsyncppal_def.py
@@ -24,9 +24,7 @@ class flsyncppal(interna):
 
         qsatype.FLSqlQuery().execSql("DELETE FROM yb_log WHERE cliente = '{}' AND tipo = '{}' AND timestamp < '{}'".format(customer, process, tsDel), "yeboyebo")
 
-        grupoprocesos = qsatype.FLUtil.sqlSelect("yb_procesos", "grupoprocesos", "cliente = '{}' AND proceso = '{}'".format(customer, process), "yeboyebo")
-
-        qsatype.FLSqlQuery().execSql("INSERT INTO yb_log (texto, cliente, tipo, grupoprocesos, timestamp) VALUES ('{}', '{}', '{}', '{}', '{}')".format(text, customer, process, grupoprocesos, tmstmp), "yeboyebo")
+        qsatype.FLSqlQuery().execSql("INSERT INTO yb_log (texto, cliente, tipo, grupoprocesos, timestamp) VALUES ('{}', '{}', '{}', (select grupoprocesos from yb_procesos where proceso = '{}'), '{}')".format(text, customer, process, process, tmstmp), "yeboyebo")
 
     def flsyncppal_replace(self, string):
         if string is None or not string or string == "":
